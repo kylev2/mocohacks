dfb1e79b25eae826d6e7e9b4dff29c1b8f702796
Esketit! <37747855+The-Big-Banana@users.noreply.github.com>
2021-02-13 14:22:33 -0500

    yes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import Main from './components/Main';
-import About from './components/About'
-import Schedules from "./components/Schedules"
-import FAQ from "./components/FAQ"
-import Sponsors from "./components/Sponsors"
-import Prizetracks from "./components/Prizetracks"
+import Main from '../components/Main';
+import About from '../components/About'
+import Schedules from "../components/Schedules"
+import FAQ from "../components/FAQ"
+import Sponsors from "../components/Sponsors"
+import Prizetracks from "../components/Prizetracks"
 //import Team from "./components/Team"
 
 function App() {

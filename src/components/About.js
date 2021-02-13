aeea885834772fe52a181a82ef9079d5164631a8
Esketit! <37747855+The-Big-Banana@users.noreply.github.com>
2021-02-13 16:06:00 -0500

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -18,7 +18,7 @@ function About() {
               </p>
             </Fade>
           </div>
-          <iframe title="MoCoHacks Video" src="https://www.youtube.com/watch?v=mY7rK9ma0h4" className="youtube" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen="allowfullscreen"
+          <iframe title="MoCoHacks Video" src="https://www.youtube.com/embed/mY7rK9ma0h4" className="youtube" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen="allowfullscreen"
             mozallowfullscreen="mozallowfullscreen"
             msallowfullscreen="msallowfullscreen"
             oallowfullscreen="oallowfullscreen"

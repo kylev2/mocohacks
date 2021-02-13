4b47c96d9ba7c2a06d017f3b78e9b3ba68ae8168
Esketit! <37747855+The-Big-Banana@users.noreply.github.com>
2021-02-13 14:26:02 -0500

    yes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import Main from '../components/Main';
-import About from '../components/About'
-import Schedules from "../components/Schedules"
-import FAQ from "../components/FAQ"
-import Sponsors from "../components/Sponsors"
-import Prizetracks from "../components/Prizetracks"
-//import Team from "./components/Team"
+import Main from './components/Main';
+import About from './components/About'
+import Schedules from "./components/Schedules"
+import FAQ from "./components/FAQ"
+import Sponsors from "./components/Sponsors"
+import Prizetracks from "./components/Prizetracks"
+import Team from "./components/Team"
 
 function App() {
   return (
--- src/components/Sponsors.js
+++ src/components/Sponsors.js
@@ -7,10 +7,10 @@ function Sponsors() {
     <Fade>
       <center>
         <h1 className="title">Partners and Sponsors</h1>
-        <img alt="bank" src={bank} />
+        <img src={bank} />
         <p className="para">
           Interested in sponsoring us? Email us at:
-        </p>
+            </p>
         <a href="mailto:team@mocohacks.org">team@mocohacks.org</a>
         <br />
         <a href="./MoCoHacks Virtual 2021_ Prospectus.pdf" download="MoCoHacks Virtual 2021_ Prospectus.pdf">Download our prospectus here!</a>

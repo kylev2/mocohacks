3c975c0d9944b0c17d72476ac37ee84fd0eeac1e
Esketit! <37747855+The-Big-Banana@users.noreply.github.com>
2021-02-13 13:53:45 -0500

    test

--- src/components/Sponsors.js
+++ src/components/Sponsors.js
@@ -4,18 +4,16 @@ import bank from "../images/Logos/bank.svg";
 
 function Sponsors() {
   return (
-    <Fade>
-      <center>
-        <h1 className="title">Partners and Sponsors</h1>
-        <img src={bank} />
-        <p className="para">
-          Interested in sponsoring us? Email us at:
-            </p>
-        <a href="mailto:team@mocohacks.org">team@mocohacks.org</a>
-        <br />
-        <a href="./MoCoHacks Virtual 2021_ Prospectus.pdf" download="MoCoHacks Virtual 2021_ Prospectus.pdf">Download our prospectus here!</a>
-      </center>
-    </Fade>
+    <center>
+      <h1 className="title">Partners and Sponsors</h1>
+      <img src={bank} />
+      <p className="para">
+        Interested in sponsoring us? Email us at:
+          </p>
+      <a href="mailto:team@mocohacks.org">team@mocohacks.org</a>
+      <br />
+      <a href="./MoCoHacks Virtual 2021_ Prospectus.pdf" download="MoCoHacks Virtual 2021_ Prospectus.pdf">Download our prospectus here!</a>
+    </center>
   )
 }
 

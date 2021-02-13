26d1f10d1369b49a861c125202421de5be68933a
Esketit! <37747855+The-Big-Banana@users.noreply.github.com>
2021-02-13 15:01:00 -0500

    yes

--- src/App.js
+++ src/App.js
@@ -5,18 +5,23 @@ import Schedules from "./components/Schedules"
 import FAQ from "./components/FAQ"
 import Sponsors from "./components/Sponsors"
 import Prizetracks from "./components/Prizetracks"
+import { HashRouter, Route, Switch } from "react-router-dom";
 import Team from "./components/Team"
 
 function App() {
   return (
-    <div className="App">
-      <Main />
-      <About />
-      <Prizetracks />
-      <Schedules />
-      <FAQ />
-      <Sponsors />
-      {/* <Team /> */}
+    <div>
+      <HashRouter basename={process.env.PUBLIC_URL}>
+        <div className="App">
+          <Main exact path="/" />
+          <About exact path="/About"/>
+          <Prizetracks exact path="/Prizetracks"/>
+          <Schedules exact path="/Schedules"/>
+          <FAQ exact path="/FAQ"/>
+          <Sponsors exact path="/Sponsors"/>
+          {/* <Team /> */}
+        </div>
+      </HashRouter>
     </div>
   );
 }

fbca83ac1dbaa7cd22a7bcdc4a8dd64949055821
Michael Wang <michaelwwang17@gmail.com>
2021-03-16 15:41:57 -0400

    add brivo in folder and add logo on navbar

--- src/components/Main.js
+++ src/components/Main.js
@@ -8,7 +8,7 @@ function Main() {
       <div className="landing">
         <div className="heading">
           <h1>MoCoHacks</h1>
-          {/*<h3 style={{ marginTop: "-30px",paddingBottom:"20px" }}><i>cohosted by MCEDC</i></h3>*/}
+          <h3 style={{ marginTop: "-30px",paddingBottom:"20px" }}><i>Reach New Heights</i></h3>
           <p>Virtual, MCPS Students</p>
           <p style={{ color: "#EDA803" }}>March 19-21, 2021</p>
           <a href=" http://bit.ly/mch-register2021" target="_blank"><button className="register">Register Here!</button></a>
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import '../styles/NavBar.css'
 import { Link } from 'react-scroll'
+import logo from "../images/icons/mch-virtual-clearbg.png"
 
 function NavBar() {
   return (
+    <>
+    <img src={logo} className="logo" alt="logo"></img>
     <div className="NavBar">
       <div className="list">
         <ul>
+          
           <li><Link className="link" to="about" smooth="true" duration={500}>About</Link></li>
           <li><Link className="link" to="tracks" smooth="true" duration={500} offset={75}> Tracks</Link></li>
           <li><Link className="link" to="schedules" smooth="true" duration={500}>Schedule</Link></li>
@@ -15,6 +19,7 @@ function NavBar() {
         </ul>
       </div>
     </div >
+    </>
   )
 }
 

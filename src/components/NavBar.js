5c74c72f740d8fcd4af2d1b6d5b64b5e8f04120c
Kyle Tu <kvt2.123@gmail.com>
2021-03-16 18:32:32 -0400

    added banner

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,23 +2,27 @@ import React from 'react';
 import '../styles/NavBar.css'
 import { Link } from 'react-scroll'
 import logo from "../images/icons/mch-virtual-clearbg.png"
+import banner from "../images/mchacks-banner.svg";
 
 function NavBar() {
   return (
     <>
-    <img src={logo} className="logo" alt="logo"></img>
-    <div className="NavBar">
-      <div className="list">
-        <ul>
-          
-          <li><Link className="link" to="about" smooth="true" duration={500}>About</Link></li>
-          <li><Link className="link" to="tracks" smooth="true" duration={500} offset={75}> Tracks</Link></li>
-          <li><Link className="link" to="schedules" smooth="true" duration={500}>Schedule</Link></li>
-          <li><Link className="link" to="faqs" smooth="true" duration={500} offset={-75}>FAQs</Link></li>
-          <li><Link className="link" to="sponsors" smooth="true" duration={500}>Sponsors</Link></li>
-        </ul>
-      </div>
-    </div >
+      <div className="NavBar">
+        <a href="https://hackclub.com/">
+          <img className="banner" src={banner}></img>
+        </a>
+        <div className="end">
+          <div className="list">
+            <ul>
+              <li><Link className="link" to="about" smooth="true" duration={500}>About</Link></li>
+              <li><Link className="link" to="tracks" smooth="true" duration={500} offset={75}> Tracks</Link></li>
+              <li><Link className="link" to="schedules" smooth="true" duration={500}>Schedule</Link></li>
+              <li><Link className="link" to="faqs" smooth="true" duration={500} offset={-75}>FAQs</Link></li>
+              <li><Link className="link" to="sponsors" smooth="true" duration={500}>Sponsors</Link></li>
+            </ul>
+          </div>
+        </div>
+      </div >
     </>
   )
 }

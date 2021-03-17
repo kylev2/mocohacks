82e34c2fa62ef6c4819ccce247f2babca95e37b2
Kyle Tu <kvt2.123@gmail.com>
2021-03-17 17:58:04 -0400

    added cards

--- src/components/Prizetracks.js
+++ src/components/Prizetracks.js
@@ -6,58 +6,85 @@ function Prizetracks() {
     return (
         <div className="body tracks">
             <div className="bg-color">
-                <div className="page page-2">
-                    <div className="para-video para-video-2">
-                        <h1 className="title">Project Categories</h1>
-                        <p className="para para-2">
-                            <h3>
-                                Best Overall Hack
-                        </h3>
-                        Best overall projects (1st, 2nd, and 3rd place). Prize: TBD
-                    </p>
-                        <br></br>
-                        <p className="para para-2">
-                            <h3>
-                                Best Beginners Hack
-                        </h3>
-                        Awarded to best project created by students attending their first hackathon (all students in the team must be beginners). Prize: TBD
-                    </p>
-                        <br></br>
-                        <p className="para para-2">
-                            <h3>
-                                Most Technically Impressive
-                        </h3>
-                        The project that utilizes the most advanced technology. Prize: TBD
-                    </p>
-                        <br></br>
-                        <p className="para para-2">
-                            <h3>
-                                Best UI
-                        </h3>
-                        The project with the most intuitive and seamless UI. Prize: $100 and UI externship (and full-stack development mentorship) opportunities with a worker at California State University (East Bay).
-                    </p>
-                        <br></br>
-                        <p className="para para-2">
-                            <h3>
-                                Best Education Hack
-                        </h3>
-                        As students, education is a crucial part of our lives. Create a hack that solves a problem in this community. Prize: TBD
-                    </p>
-                        <br></br>
-                        <p className="para para-2">
-                            <h3>
-                                Best Social Good Hack
-                        </h3>
-                        In many ways, our social environment shapes who we are. Create a hack that solves a problem in the community. Prize: TBD
-                    </p>
-                        <br></br>
-                        <p className="para para-2">
-                            <h3>
-                                Most Humorous Hack
-                        </h3>
-                        Make us laugh. PERIODT. Prize: TBD
-                    </p>
-                        <br></br>
+                <div className="page">
+                    <div className="card">
+                        <div className="card-front">
+                            <p className="text">Best Overall Hacks (1st, 2nd, 3rd place):</p>
+                            <p className="text">
+                                The Best Overall Hacks showcase exceptional thought, creativity, and mission pertaining to MoCoHacks’s theme this year: Reach New Heights! For this category, three teams will be picked as winners.
+                            </p>
+                            <div className="text">
+                                <p>
+                                    1st Place Prize: Apple AirPod 2’s and Point3 Subscription for 6 months
+                                </p>
+                                <p>
+                                    2nd Place Prize: Amazon Echo Show 5 & Point3 Subscription for 3 months
+                                </p>
+                                <p>
+                                    3rd Place Prize: $35 Amazon Gift Card and Point3 Subscription for 1 month
+                                </p>
+                            </div>
+                        </div>
+                        <div className="card-front">
+                            <p className="text">Best Beginners Hack:</p>
+                            <p className="text">
+                                Best project made by attendees participating as a beginner (All members on the team must be registered as a Beginner).
+                            </p>
+                            <p className="text">
+                                Prize: HyperX Cloud Stinger Gaming Headset and ESCALATE Swag Pack
+                            </p>
+                        </div>
+                        <div className="card-front">
+                            <p className="text">Most Technically Impressive:</p>
+                            <p className="text">
+                                The most advanced use of technology and/or software.
+                            </p>
+                            <p className="text">
+                                Prize: Logitech G915 Lightspeed Keyboard and ESCALATE Subscription for 1 month.
+                            </p>
+                        </div>
+                        <div className="card-front">
+                            <p className="text">Best UI:</p>
+                            <p className="text">
+                                The project with the most intuitive and seamless UI.
+                            </p>
+                            <p className="text">
+                                Prize: HyperX Fury Ultra RGB Gaming Mouse Pad, $100 with a UI/graphic design externship offer, and ESCALATE Swag Pack.
+                            </p>
+                        </div>
+                        <div className="card-front">
+                            <p className="text">Best Social Good Hack:</p>
+                            <p className="text">
+                                In many ways, our social environment shapes who we are. Create a hack that solves a problem in the community.
+                            </p>
+                            <p className="text">
+                                Prize: FujiFilm Instax Mini 9 Camera and ESCALATE Swag Pack.
+                            </p>
+                        </div>
+                        <div className="card-front">
+                            <p className="text">Best Education Hack:</p>
+                            <p className="text">
+                                As students, education is a crucial part of our lives. Create a hack that pertains to student life or education.
+                            </p>
+                            <p className="text">
+                                Prize: Logitech G502 Wireless Mouse and a Copy of 5th Domain Book
+                            </p>
+                        </div>
+                        <div className="card-front">
+                            <p className="text">Most Entertaining Hack:</p>
+                            <p className="text">
+                                Make us laugh. PERIOD (Must be appropriate).
+                            </p>
+                            <p className="text">
+                                Prize: Portable Movie Projector and ESCALATE Swag Pack.
+                            </p>
+                        </div>
+                        <div className="card-front">
+                            <p className="text">Organizers Honorable Mention:</p>
+                            <p className="text">
+                                Prize: $20 Amazon Gift Card
+                            </p>
+                        </div>
                     </div>
                 </div>
             </div>

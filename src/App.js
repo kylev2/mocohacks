eec0863e67e828d131aca5ed7a0075e3d22c148f
Kyle Tu <kvt2.123@gmail.com>
2021-03-18 19:10:40 -0400

    added some cardinos

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Sponsors from "./components/Sponsors"
 import Prizetracks from "./components/Prizetracks"
 import DocumentMeta from 'react-document-meta';
 import { Animator, ScrollContainer, ScrollPage, batch, FadeIn, MoveOut, StickyIn, ZoomIn, FadeUp } from "react-scroll-motion";
+import { Navbar } from 'react-bootstrap';
 //import Team from "./components/Team"
 
 function App() {
@@ -27,79 +28,12 @@ function App() {
     <DocumentMeta {...meta}>
       <div className="App">
         <Main />
-        <ScrollContainer>
-          <ScrollPage page={1}>
-            <Animator animation={FadeUp}>
-              <About />
-            </Animator>
-          </ScrollPage>
-          <ScrollPage page={2}>
-            <Animator animation={ZoomInScrollOut}>
-              <span style={{ fontSize: "3em" }}>Reach New Heights.</span>
-            </Animator>
-          </ScrollPage>
-          <ScrollPage page={3}>
-            <Animator animation={ZoomInScrollOut}>
-              <span style={{ fontSize: "3em" }}>Ever had that one idea?</span>
-              <span style={{ fontSize: "1em", marginTop: "50px", opacity: "50%" }}>
-                <Animator animation={MoveOut(320, 0)}>an app?</Animator>
-                <Animator animation={MoveOut(-240, 0)}>or business?</Animator>
-                <Animator animation={MoveOut(750, 240)}>or website?</Animator>
-              </span>
-            </Animator>
-          </ScrollPage>
-          <ScrollPage page={4}>
-            <Animator animation={ZoomInScrollOut}>
-              <span style={{ fontSize: "3em" }}>Form A Team<br></br></span>
-              <span style={{ fontSize: "1em", marginTop: "50px"}}>
-                From peers to mentors to industry experts, there will be no shortage of people you can meet! Make a team with new friends (or friends you already have) that have similar interests. Combine brainpower to create a project together!
-              </span>
-            </Animator>
-          </ScrollPage>
-          <ScrollPage page={5}>
-            <Animator animation={ZoomInScrollOut}>
-              <span style={{ fontSize: "3em" }}>Workshops<br></br></span>
-              <span style={{ fontSize: "1em", marginTop: "50px"}}>
-              Learn programming skills at the various workshops hosted at MoCoHacks. Use these skills to help you along with your project! There will also be talks and mini-tournaments you can participate in while taking a break from coding!
-              </span>
-            </Animator>
-          </ScrollPage>
-          <ScrollPage page={6}>
-            <Animator animation={ZoomInScrollOut}>
-              <span style={{ fontSize: "3em" }}>Present<br></br></span>
-              <span style={{ fontSize: "1em", marginTop: "50px"}}>
-              At the end of the hackathon, present your project to win prizes! Look below to find the project tracks for this year! 
-              </span>
-            </Animator>
-          </ScrollPage>
-          <ScrollPage page={7}>
-            <Animator animation={ZoomInScrollOut}>
-              <span style={{ fontSize: "3em" }}>Conclusion<br></br></span>
-              <span style={{ fontSize: "1em", marginTop: "50px"}}>
-              We hope you take this opportunity to learn something new, meet new people, and have fun. We hope you take this opportunity to: <br></br>
-              <b>Reach New Heights!</b>
-              </span>
-            </Animator>
-          </ScrollPage>
-          <ScrollPage page={8}>
-            <Prizetracks />
-          </ScrollPage>
-          <ScrollPage page={9}>
-            <center>
-              <Speakers />
-            </center>
-          </ScrollPage>
-          <ScrollPage page={10}>
-            <Schedules />
-          </ScrollPage>
-          <ScrollPage page={11}>
-            <FAQ />
-          </ScrollPage>
-          <ScrollPage page={12}>
-            <Sponsors />
-          </ScrollPage>
-          {/* <Team /> */}
-        </ScrollContainer>
+        <About />
+        <Prizetracks />
+        <Speakers />
+        <Schedules />
+        <FAQ />
+        <Sponsors />
       </div>
     </DocumentMeta>
   );
--- src/components/Speakers.js
+++ src/components/Speakers.js
@@ -1,8 +1,6 @@
 import React from "react"
 
-import '../styles/Speakers.css'
-import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
-import { Carousel } from 'react-responsive-carousel';
+import '../styles/Prizetracks.css'
 
 import Glass from '../images/speakers/glass.png';
 import Han from '../images/speakers/han.png';
@@ -13,35 +11,74 @@ import Wu from '../images/speakers/wu.png';
 
 function Speakers() {
     return (
-        <div>
-            <h1 className="title">Keynote Speakers</h1>
-            <div style={{width: "50vh"}}>
-                <Carousel>
-                    <div>
-                        <img src={Latta} />
-                        <p className="legend">Zack Latta <br></br> Founder and Executive Director of Hack Club. Thiel Fellow; Forbes 30 Under 30 List.</p>
+        <div className="body tracks">
+            <div className="card">
+                <div className="card-front">
+                    <div className="align-circle">
+                        <div className="circle">
+                            <img src={Latta} className="scale"></img>
+                        </div>
                     </div>
-                    <div>
-                        <img src={Han} />
-                        <p className="legend">Tyler Han <br></br> Co-Founder and CTO of Voiceflow</p>
+                    <b><p className="text" style={{ fontSize: "1.5em" }}>Zack Latta</p></b>
+                    <p className="text">
+                        Founder and Executive Director of Hack Club. Thiel Fellow; Forbes 30 Under 30 List.
+                    </p>
+                </div>
+                <div className="card-front">
+                    <div className="align-circle">
+                        <div className="circle">
+                            <img src={Latta} className="scale"></img>
+                        </div>
                     </div>
-                    <div>
-                        <img src={Wu} />
-                        <p className="legend">Benjamin Wu <br></br> President and CEO of MCEDC</p>
+                    <b><p className="text" style={{ fontSize: "1.5em" }}>Zack Latta</p></b>
+                    <p className="text">
+                        Founder and Executive Director of Hack Club. Thiel Fellow; Forbes 30 Under 30 List.
+                    </p>
+                </div>
+                <div className="card-front">
+                    <div className="align-circle">
+                        <div className="circle">
+                            <img src={Latta} className="scale"></img>
+                        </div>
                     </div>
-                    <div>
-                        <img src={Jawando} />
-                        <p className="legend">Will Jawando <br></br> MoCo Councilmember (at-large) since 2018. Attorney, activist, and community leader with lifelong dedication to public service.</p>
+                    <b><p className="text" style={{ fontSize: "1.5em" }}>Zack Latta</p></b>
+                    <p className="text">
+                        Founder and Executive Director of Hack Club. Thiel Fellow; Forbes 30 Under 30 List.
+                    </p>
+                </div>
+                <div className="card-front">
+                    <div className="align-circle">
+                        <div className="circle">
+                            <img src={Latta} className="scale"></img>
+                        </div>
                     </div>
-                    <div>
-                        <img src={Wang} />
-                        <p className="legend">Nancy Wang <br></br> GM of Data Protection Services @ Amazon AWS. Angel investor. UPenn CS grad. CEO of Advancing Women in Tech (AWIT)</p>
+                    <b><p className="text" style={{ fontSize: "1.5em" }}>Zack Latta</p></b>
+                    <p className="text">
+                        Founder and Executive Director of Hack Club. Thiel Fellow; Forbes 30 Under 30 List.
+                    </p>
+                </div>
+                <div className="card-front">
+                    <div className="align-circle">
+                        <div className="circle">
+                            <img src={Latta} className="scale"></img>
+                        </div>
                     </div>
-                    <div>
-                        <img src={Glass} />
-                        <p className="legend">Evan Glass <br></br> MoCo Councilmember (at-large) since 2018; first LGBTQ+ member.</p>
+                    <b><p className="text" style={{ fontSize: "1.5em" }}>Zack Latta</p></b>
+                    <p className="text">
+                        Founder and Executive Director of Hack Club. Thiel Fellow; Forbes 30 Under 30 List.
+                    </p>
+                </div>
+                <div className="card-front">
+                    <div className="align-circle">
+                        <div className="circle">
+                            <img src={Latta} className="scale"></img>
+                        </div>
                     </div>
-                </Carousel>
+                    <b><p className="text" style={{ fontSize: "1.5em" }}>Zack Latta</p></b>
+                    <p className="text">
+                        Founder and Executive Director of Hack Club. Thiel Fellow; Forbes 30 Under 30 List.
+                    </p>
+                </div>
             </div>
         </div>
     )
